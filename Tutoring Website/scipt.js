b4b44bfff6c6d8cb8389f933aafca1d2c6d9965c
Senthedon <housseinalialmawla@gmail.com>
2025-07-12 12:04:16 +0300

    Add files via upload

---
+++ Tutoring Website/scipt.js
@@ -0,0 +1,17 @@
+// Scroll animation for sections
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2,
+});
+
+sections.forEach(section => {
+  section.classList.add("hidden");
+  observer.observe(section);
+});
